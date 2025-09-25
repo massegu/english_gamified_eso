1974742b547c464aae63e9d0f1aeae3e2b08c5e0
massegu <massegu@gmail.com>
2025-09-25 10:00:19 +0000

    🔊 Eliminar carga de audio si no existe el archivo

--- streamlit_app/components/interactive_cards.py
+++ streamlit_app/components/interactive_cards.py
@@ -1,11 +1,22 @@
 import streamlit as st
+import os
 
 def show_card(image_path, word, definition, audio_path=None):
-    st.image(image_path, width=200)
+    default_image = "assets/images/personaje_1.png"
+    
+    if os.path.exists(image_path):
+        st.image(image_path, width=200)
+    else:
+        st.image(default_image, width=200)
+        st.warning(f"⚠️ Imagen no encontrada: {image_path}")
+    
     st.write(f"**Palabra:** {word}")
     
     if st.button(f"¿Qué significa '{word}'?"):
         st.info(definition)
     
-    if audio_path:
+    if audio_path and os.path. exists(audio_path):
         st.audio(audio_path)
+
+
+
